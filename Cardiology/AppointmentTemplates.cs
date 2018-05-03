ec7fe1d6746289530dc1d9403b1bffceb5b537a7
tulkas <1@id-mt.ru>
2018-05-03 17:13:14 +0300

    Fix #29. Форма переименована в AppointmentTemplates

---
+++ Cardiology/AppointmentTemplates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cardiology
+{
+    public partial class AppointmentTemplates : Form
+    {
+        public AppointmentTemplates()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AppointmentTemplates_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

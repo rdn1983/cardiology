7c2300c18a7b7e46bc81403a80d2bec97f18b60b
BotsanovaSS <s.botsanova@id-mt.ru>
2018-05-06 17:31:32 +0300

    Fix #3

---
+++ Cardiology/DB3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cardiology
+{
+    public partial class DB3 : Form
+    {
+        public DB3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DB3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
